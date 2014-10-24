2e43b9dae93e5c960169d2b0464cf3e66d3597af
iarmoa <isaias.armoa@gmail.com>
2014-10-24 14:07:41 -0300

    tablas agregadas: blocks (manzanas) groups (grupos)
    tablas modificadas:
    neighborhoods_pkey

--- app/sql/init.sql
+++ app/sql/init.sql
@@ -38,10 +38,95 @@ CREATE TABLE neighborhoods
   departament integer,
   name character varying,
   leader_id integer,
-  CONSTRAINT neighborhoods_pkey PRIMARY KEY (zone)
+  CONSTRAINT neighborhoods_pkey PRIMARY KEY (id)
 )
 WITH (
   OIDS=FALSE
 );
 ALTER TABLE neighborhoods
   OWNER TO postgres;
+
+
+CREATE TABLE blocks
+(
+  id serial NOT NULL,
+  name character varying NOT NULL,
+  neighborhood_id integer NOT NULL,
+  group_id integer,
+  created time without time zone,
+  modified time without time zone,
+  user_id integer,
+  CONSTRAINT blocks_pkey PRIMARY KEY (id),
+  CONSTRAINT blocks_fkey_groups FOREIGN KEY (group_id)
+      REFERENCES groups (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT blocks_fkey_neighborhoods FOREIGN KEY (neighborhood_id)
+      REFERENCES neighborhoods (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT blocks_user_id FOREIGN KEY (user_id)
+      REFERENCES users (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE blocks
+  OWNER TO postgres;
+
+-- Index: fki_blocks_fkey_groups
+
+-- DROP INDEX fki_blocks_fkey_groups;
+
+CREATE INDEX fki_blocks_fkey_groups
+  ON blocks
+  USING btree
+  (group_id);
+
+-- Index: fki_blocks_fkey_neighborhoods
+
+-- DROP INDEX fki_blocks_fkey_neighborhoods;
+
+CREATE INDEX fki_blocks_fkey_neighborhoods
+  ON blocks
+  USING btree
+  (neighborhood_id);
+
+-- Index: fki_blocks_user_id
+
+-- DROP INDEX fki_blocks_user_id;
+
+CREATE INDEX fki_blocks_user_id
+  ON blocks
+  USING btree
+  (user_id);
+
+
+
+CREATE TABLE groups
+(
+  id serial NOT NULL,
+  name character varying NOT NULL,
+  user_id integer,
+  created time without time zone,
+  modified time without time zone,
+  leader_id integer NOT NULL,
+  people_id integer NOT NULL,
+  CONSTRAINT groups_pkey PRIMARY KEY (id),
+  CONSTRAINT groups_fkey_users FOREIGN KEY (user_id)
+      REFERENCES users (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE groups
+  OWNER TO postgres;
+
+-- Index: fki_groups_fkey_users
+
+-- DROP INDEX fki_groups_fkey_users;
+
+CREATE INDEX fki_groups_fkey_users
+  ON groups
+  USING btree
+  (user_id);
